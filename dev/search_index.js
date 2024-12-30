bb7a8de4f8b9b8133c06f144d4ffbfa1cf6cc0f6
Documenter.jl <documenter@juliadocs.github.io>
2024-12-30 09:01:02 +0000

    build based on 0745e08

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HodgeDiamonds","category":"page"},{"location":"#HodgeDiamonds","page":"Home","title":"HodgeDiamonds","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HodgeDiamonds.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HodgeDiamonds]","category":"page"}]
+}
